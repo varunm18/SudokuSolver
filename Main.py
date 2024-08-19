b0fb8cbd15227d8366befddea01e15357a89542a
varunm18 <varun.mannam18@gmail.com>
2024-08-19 11:56:05 -0400

    added command line args and improved efficiency of solver with automatic assignment. made all squares variables

--- Main.py
+++ Main.py
@@ -1,34 +1,29 @@
-from Solver import NaiveBacktrack, SudokuCSP
+import argparse
+from Solver import NaiveBacktrack, ConstraintPropagation
 from sudoku import Sudoku
 
-def read_board():
-    return [[5, None, None,  None, None, 5,  None, 8, None],
-    [None, None, None,  6,    None, 1,  None, 4, 3],
-    [None, None, None,  None, None, None, None, None, None],
-    [None, 1,    None,  5,    None, None, None, None, None],
-    [None, None, None,  1,    None, 6,  None, None, None],
-    [3,    None, None,  None, None, None, None, None, 5],
-    [5,    3,    None,  None, None, None, None, 6, 1],
-    [None, None, None,  None, None, None, None, None, 4],
-    [None, None, None,  None, None, None, None, None, None]]
-
 def main():
-    # board = Sudoku(3).difficulty(0.99).board
-    # solver = NaiveBacktrack(board)
-    # solver.solve()
+    parser = argparse.ArgumentParser()
 
-    board = Sudoku(3).difficulty(0.6).board
-    solver = SudokuCSP(board, backtrack_inference=True, minimum_remaining_values=True, least_constraining_values=True)
-    solver.solve()
-    solver = SudokuCSP(board, backtrack_inference=True, minimum_remaining_values=True)
-    solver.solve()
-    solver = SudokuCSP(board, backtrack_inference=False, minimum_remaining_values=False, least_constraining_values=True)
-    solver.solve()
-    solver = SudokuCSP(board, backtrack_inference=True)
-    solver.solve()
-    solver = SudokuCSP(board, backtrack_inference=False)
-    solver.solve()
-    solver = NaiveBacktrack(board)
+    parser.add_argument("-i", "--Input", help="Optional Input File")
+    parser.add_argument("--naive", help="Use Naive Backtracking", action="store_true")
+    parser.add_argument("--inference", help="Use Inference when Backtracking", action="store_true")
+    parser.add_argument("--mrv", help="Use Minimum Remaining Values", action="store_true")
+    parser.add_argument("--lcv", help="Use Least Constraining Values", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.naive:
+        solver = NaiveBacktrack(file=args.Input)
+    else:
+        inference = mrv = lcv = False
+        if args.inference:
+            inference = True
+        if args.mrv:
+            mrv = True
+        if args.lcv:
+            lcv = True
+        solver = ConstraintPropagation(file=args.Input, backtrack_inference=inference, minimum_remaining_values=mrv, least_constraining_values=lcv)
     solver.solve()
 
 if __name__ == "__main__":
--- Solver.py
+++ Solver.py
@@ -1,13 +1,32 @@
+from sudoku import Sudoku
+
 class InvalidBoard(Exception): pass
 
 class SudokuSolver():
-    def __init__(self, board):
+    def __init__(self, file=None):
         # deepcopy
-        self.original = [row.copy() for row in board]
-        self.board = [row.copy() for row in board]
+        if file is None:
+            self.board = Sudoku(3).difficulty(0.5).board
+        else:
+            self.board = self.read_board(file)
+        self.original = [row.copy() for row in self.board]
+
         if not self.is_valid():
             raise InvalidBoard("Inputed Sudoku board is not valid")
+        
         self.steps = 0
+
+    def read_board(self, file):
+        board = []
+        with open(file, 'r') as f:
+            for row in f:
+                board.append([])
+                for num in row.strip():
+                    if int(num)==0:
+                        board[-1].append(None)
+                    else:
+                        board[-1].append(int(num))
+        return board
     
     def valid_assignment(self, row, col, num):
         for i in range(9):
@@ -74,8 +93,8 @@ class SudokuSolver():
         print()
     
 class NaiveBacktrack(SudokuSolver):
-    def __init__(self, board):
-        super().__init__(board)
+    def __init__(self, file=None):
+        super().__init__(file)
 
     def solve(self):
         if self.backtrack():
@@ -107,85 +126,83 @@ class NaiveBacktrack(SudokuSolver):
                     return row, col
         return None, None
 
-
-class SudokuCSP(SudokuSolver):
-    def __init__(self, board, backtrack_inference=False, minimum_remaining_values=False, least_constraining_values=False):
-        super().__init__(board)
+class ConstraintPropagation(SudokuSolver):
+    def __init__(self, file=None, backtrack_inference=False, minimum_remaining_values=False, least_constraining_values=False):
+        super().__init__(file)
 
         # store unassigned variables
         self.variables = dict()
         for row in range(9):
             for col in range(9):
-                if self.board[row][col] is None:
-                    self.variables[(row, col)] = self.Variable(row, col)
-                    self.variables[(row, col)].limit_domain(self.board)
+                self.variables[(row, col)] = self.Variable(row, col)
+                self.variables[(row, col)].limit_domain(self.board)
         
-        self.constraints = dict()
         for var in self.variables.values():
-            self.constraints[var] = self.get_constraints(var)
+            var.constraints = self.get_constraints(var)
 
         self.inference = backtrack_inference
         self.mrv = minimum_remaining_values
         self.lcv = least_constraining_values
-         
-
-    def solve(self):
-        self.ac3()
-        if self.backtrack():
-            print(f"Found Solution in {self.steps} steps\n")
-            # self.print_board(original=True)
-            # self.print_board()
-        else:
-            print(f"No Solution Found after {self.steps} steps\n")
- 
-    def ac3(self, arcs=None):
-        queue = []
-        if arcs is None:
-            for var in self.variables.values():
-                for constraint in self.constraints[var]:
-                    if (constraint, var) not in queue:
-                        queue.append((var, constraint))
-        else:
-            queue = arcs
-        
-        while len(queue)>0:
-            (x, y) = queue.pop(0)
-            if self.revise(x, y):
-                if len(x.domain)==0:
-                    return False
-                for z in (self.constraints[x] - {y}):
-                    queue.append((z, x))
-        return True
     
     def get_constraints(self, var):
         constraints = set()
         for i in range(9):
             # horizontal
-            if i != var.col and (var.row, i) in self.variables:
+            if i != var.col:
                 constraints.add(self.variables[(var.row, i)])
             # vertical
-            if i != var.row and (i, var.col) in self.variables:
+            if i != var.row:
                 constraints.add(self.variables[(i, var.col)])
         # square
         for i in range(3):
             for j in range(3):
                 shift_hor = var.row // 3 * 3
                 shift_ver = var.col // 3 * 3
-                if (i+shift_hor!=var.row and j+shift_ver!=var.col) and (i+shift_hor, j+shift_ver) in self.variables:
+                if (i+shift_hor!=var.row and j+shift_ver!=var.col):
                     constraints.add(self.variables[(i+shift_hor, j+shift_ver)])
         return constraints
 
+    def solve(self):
+        if self.ac3():
+
+            backtrack = False
+            for (row, col) in self.variables:
+                if len(self.variables[(row, col)].domain)==1:
+                    self.board[row][col] = self.variables[(row, col)].domain[0]
+                else:
+                    backtrack = True
+
+            # not backtrack -> solution already found 
+            if not backtrack or self.backtrack():
+                print(f"Found Solution in {self.steps} steps\n")
+                self.print_board(original=True)
+                self.print_board()
+            else:
+                print(f"No Solution Found after {self.steps} steps\n")
+ 
+    def ac3(self, arcs=None):
+        if arcs is None:
+            arcs = []
+            for var in self.variables.values():
+                for constraint in var.constraints:
+                    arcs.append((var, constraint))
+        
+        while len(arcs)>0:
+            (x, y) = arcs.pop(0)
+            if self.revise(x, y):
+                if len(x.domain)==0:
+                    return False
+                for z in (x.constraints - {y}):
+                    arcs.append((z, x))
+        return True
+    
     def revise(self, x, y):
         # Makes x arc consisitent with y, statisfies binary constraints
         revised = False
         copy = x.domain.copy()
 
         for x_val in copy:
-            delete = True
-            for y_val in y.domain:
-                if x_val != y_val:
-                    delete = False
-            if delete:
+            if not any([x_val!=y_val for y_val in y.domain]):
                 x.domain.remove(x_val)
                 revised = True
         
@@ -207,7 +224,7 @@ class SudokuCSP(SudokuSolver):
                 self.board[var.row][var.col] = num
                 
                 if self.inference:
-                    self.ac3([(y, var) for y in self.constraints[var]])
+                    self.ac3([(y, var) for y in var.constraints])
 
                 if self.backtrack():
                     return True
@@ -218,6 +235,22 @@ class SudokuCSP(SudokuSolver):
                     self.reset_domain(original_domains)
         return False 
     
+    def find_unassigned_variable(self):
+        # if mrv choose based on smallest domain and highest degree if tied
+        if self.mrv:
+            unassigned = []
+            for var in self.variables.values():
+                if len(var.domain) > 1 and self.board[var.row][var.col] is None:
+                    unassigned.append(var)
+                elif len(var.domain) == 1:
+                    self.board[var.row][var.col] = var.domain[0]
+            return min(unassigned, key=lambda x: (len(x.domain), -len(x.constraints)), default=None)
+
+        for var in self.variables.values():
+            if len(var.domain) > 1:
+                return var
+        return None
+    
     def get_domain(self):
         domain = dict()
         for var in self.variables.values():
@@ -227,46 +260,33 @@ class SudokuCSP(SudokuSolver):
     def reset_domain(self, domain):
         for var in self.variables.values():
             var.domain = domain[var]
-    
-    def find_unassigned_variable(self):
-        # if mrv choose based on smallest domain and highest degree if tied
-        if self.mrv:
-            unassigned = [var for var in self.variables.values() if self.board[var.row][var.col] is None]
-            unassigned.sort(key = lambda x: len(self.constraints[x]), reverse=True)
-            unassigned.sort(key = lambda x: len(x.domain))
-            return unassigned[0] if len(unassigned)>0 else None
-
-        for var in self.variables.values():
-            if self.board[var.row][var.col] is None:
-                return var
-        return None
 
     def order_domain(self, var):
         # if lcv order domain vals by lowest appearance in other constrained variable domains 
         if not self.lcv:
             return var.domain
         
-        domain_constraint = {
-            val: 0
-            for val in var.domain
-        }
-
-        for val in var.domain:
-            for constraint in self.constraints[var]:
-                if self.board[constraint.row][constraint.col] is None:
-                    for val2 in constraint.domain:
-                        if val==val2:
-                            domain_constraint[val] += 1
-        return sorted(domain_constraint, key = lambda x: domain_constraint[x])
+        return sorted(var.domain, key = lambda val: self.count_conflicts(val, var))
+    
+    def count_conflicts(self, val, var):
+        count = 0
+        for constraint in var.constraints:
+                if len(constraint.domain) > 1:
+                    count += sum(1 for val2 in constraint.domain if val == val2)
+        return count
 
     class Variable():
         def __init__(self, row, col):
             self.row = row
             self.col = col
-            self.value = None
             self.domain = [x for x in range(1, 10)]
+            self.constraints = None
 
         def limit_domain(self, board):
+            if board[self.row][self.col] is not None:
+                self.domain = [board[self.row][self.col]]
+                return
+            
             for i in range(9):
                 # horizontal
                 if i != self.col and board[self.row][i] in self.domain:
@@ -283,4 +303,4 @@ class SudokuCSP(SudokuSolver):
                         self.domain.remove(board[i+shift_hor][j+shift_ver])
         
         def __repr__(self):
-            return f"{self.row}, {self.col}"
+            return f"({self.row}, {self.col})"
